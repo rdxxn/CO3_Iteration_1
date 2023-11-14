90e9767d0032a656922754726772041487404d43
rdxxn <rhys@rhysdixon.co.uk>
2023-11-14 22:35:39 +0000

    Added All the forms, giving them suitable titles.

--- Iteration 1/DifficultySelectMenu.cs
+++ Iteration 1/DifficultySelectMenu.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace Iteration_1
 {
-    public partial class Form1 : Form
+    public partial class DifficultySelectMenu : Form
     {
-        public Form1()
+        public DifficultySelectMenu()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void DifficultySelectMenu_Load(object sender, EventArgs e)
         {
 
         }
---
+++ Iteration 1/MapSelectMenu.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Iteration_1
+{
+    partial class MapSelectMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MapSelectMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MapSelectMenu";
+            this.Text = "MapSelectMenu";
+            this.Load += new System.EventHandler(this.MapSelectMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
